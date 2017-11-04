b0bf5a24868ae7f8bd569b26d4f54f2d6af8f26c
ctysingh <chaitanyasingh@gmail.com>
2017-11-04 19:19:13 -0400

    handling event inner component

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import{Component, OnChanges, Input} from '@angular/core';
+import{Component, OnChanges, Input, Output,EventEmitter} from '@angular/core';
 
 @(Component)({
     selector:'pm-star',
@@ -9,7 +9,13 @@ import{Component, OnChanges, Input} from '@angular/core';
 export class StarComponent implements OnChanges{
     @Input() rating:number ;
     starWidth:number;
-
+    @Output() ratingClicked:EventEmitter<string> = new EventEmitter<string>();
+    
+    onClick(){
+      //this.notify.emit('clicked');
+      //console.log(`the rating ${this.rating} was clicked!`);
+      this.ratingClicked.emit(`the rating ${this.rating} was clicked!`);
+    }
     ngOnChanges():void{
         this.starWidth = this.rating*86/5;
     }
