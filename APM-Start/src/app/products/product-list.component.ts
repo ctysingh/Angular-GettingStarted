0ea03b37188bb3e8774925726fbc6ff4c5e804f6
ctysingh <chaitanyasingh@gmail.com>
2017-11-04 07:40:30 -0400

    added event button click event handling

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -8,6 +8,10 @@ import {Component} from '@angular/core';
 
 export class ProductListComponent{
     pageTitle:string='Product List';
+    imageWidth:number=50;
+    imageMargin:number=2;
+    showImage:boolean=false;
+
     products:any[] = [
         {
             "productId": 2,
@@ -42,4 +46,8 @@ export class ProductListComponent{
         "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
         }
     ]
+    
+    toggleImage():void{
+        this.showImage = !this.showImage;
+    }
 }
