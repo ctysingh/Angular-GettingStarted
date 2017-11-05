ce8b4bea77cc4a00a49e11ebc88a4e5b6ece2d9d
ctysingh <chaitanyasingh@gmail.com>
2017-11-04 21:01:17 -0400

    adding product service

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {IProduct} from  './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector:'pm-products',
@@ -10,9 +11,7 @@ import {IProduct} from  './product';
 
 export class ProductListComponent implements OnInit{
 
-    ngOnInit(): void {
-        console.log("OnInit method");
-    }
+  
     pageTitle:string='Product List';
     imageWidth:number=50;
     imageMargin:number=2;
@@ -31,45 +30,13 @@ export class ProductListComponent implements OnInit{
 
     filteredProducts :IProduct[];
 
-    products:IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-       
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2016",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    products:IProduct[] = [] 
     
-        },
-        {
-        "productId": 8,
-        "productName": "Saw",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2016",
-        "description": "15-inch steel blade hand saw",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
-        }
-    ]
 
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
-    }
+    constructor(private _productService:ProductService)
+    {
+
+    }   
     
     performFilter(filterBy:string):IProduct[]{
         filterBy = filterBy.toLowerCase();
@@ -81,5 +48,11 @@ export class ProductListComponent implements OnInit{
     }
     toggleImage():void{
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        //console.log("OnInit method");
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
