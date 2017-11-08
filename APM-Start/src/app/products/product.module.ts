56179377711429718a6d6961558f45a1fddb808c
ctysingh <chaitanyasingh@gmail.com>
2017-11-07 22:57:34 -0500

    adding shared module

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,27 +1,24 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
-import { StarComponent } from '../shared/star.component';
 import { ConvertToSpaces } from '../shared/convert-to-spaces.pipe';
-import { FormsModule } from '@angular/forms';
+
 import { RouterModule } from '@angular/router';
 import { ProductService } from './product.service';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path:'products', component:ProductListComponent},
       {path:'products/:id', component:ProductDetailComponent}
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ConvertToSpaces,
-    StarComponent
+    ConvertToSpaces
   ],
   providers:[
     ProductService
